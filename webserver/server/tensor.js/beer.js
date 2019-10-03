c03a6fcf2ae25b5d19f3e9f2d41bebe6766058ac
Ian Gallgher <igallag93@gmail.com>
2019-10-03 16:14:39 -0400

    tensor flow seems to work in the correct direction

---
+++ webserver/server/tensor.js/beer.js
@@ -0,0 +1,56 @@
+const tf = require('@tensorflow/tfjs')
+const holder = require('@tensorflow/tfjs-node')
+const beerTesting = require('./beerTesting.json')
+const beer = require('./beer.json')
+
+const trainingData = tf.tensor2d(
+  beer.map(item => [item.abv, item.geo, item.type])
+)
+
+const outputData = tf.tensor2d(beer.map(item => [item.score]))
+
+const testingData = tf.tensor2d(
+  beerTesting.map(item => [item.abv, item.geo, item.type])
+)
+
+// neural network
+
+const model = tf.sequential()
+
+model.add(
+  tf.layers.dense({
+    inputShape: [3],
+    activation: 'linear',
+    units: 4
+    // kernelInitializer: "constant"
+    // useBias: true
+  })
+)
+
+model.add(
+  tf.layers.dense({
+    activation: 'linear',
+    units: 1,
+    kernelInitializer: 'ones'
+  })
+)
+
+model.add(
+  tf.layers.dense({
+    activation: 'linear',
+    units: 1,
+    kernelInitializer: 'ones'
+  })
+)
+
+model.compile({
+  loss: 'meanSquaredError',
+  optimizer: tf.train.adam(0.06)
+})
+
+// train our network
+
+model.fit(trainingData, outputData, {epochs: 1000}).then(history => {
+  console.log(history)
+  model.predict(testingData).print()
+})
