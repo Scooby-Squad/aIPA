766316f9f19aa391c7340eb385575e53b3a8a025
Dominic Sciara <dominic.sciara@gmail.com>
2019-10-04 11:19:49 -0400

    changed userBeer model

--- webserver/server/db/models/userBeer.js
+++ webserver/server/db/models/userBeer.js
@@ -7,4 +7,37 @@ const User_Beer = db.define('user_beer', {
   }
 })
 
+// Find or Creates a User_Beer and returns it
+User_Beer.get = async function(userId, beerId) {
+  const userBeer = await User_Beer.findOrCreate({
+    where: {
+      userId,
+      beerId
+    }
+  })
+  return userBeer
+}
+
+// Finds User_Beers for a user
+User_Beer.findRatings = async function(userId) {
+  const userbeers = await User_Beer.findAll({
+    where: {userId}
+  })
+  return userbeers
+}
+
+// Updates a specific User_Beer for a specific user
+User_Beer.updateRatings = async function(userId, beerId, rating) {
+  const updatedUserBeer = await User_Beer.update(
+    {rating},
+    {
+      where: {
+        userId,
+        beerId
+      }
+    }
+  )
+  return updatedUserBeer
+}
+
 module.exports = User_Beer
