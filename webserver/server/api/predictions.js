a8527c4c1e2ea40c08d4380f891f06fa4520486d
dhochbaum <dhochbaum@gmail.com>
2019-10-08 14:40:26 -0400

    tensorflow api call works, user id is hardcoded to 1 and needs to be changed eventually

--- webserver/server/api/predictions.js
+++ webserver/server/api/predictions.js
@@ -7,47 +7,30 @@ const Op = Sequelize.Op
 
 // Get Predictions for All Beers
 router.get('/', async (req, res, next) => {
+  // USER ID 1 IS HARDCODED FOR NOW!!!!!!!!!!!!!!!!!!!!!!
+
   try {
     const ratedBeers = await User_Beer.findAll({
-      where: {userId: 1, rating: {[Op.ne]: '0'}}
+      where: {userId: 1, rating: {[Op.ne]: '0'}},
+      include: [{model: Beer}]
     })
+    console.log(
+      ratedBeers[0].dataValues.beer.dataValues,
+      'RATEDBEERSDATAVALUES'
+    )
     let userRatedBeers = []
     ratedBeers.forEach(beer => {
-      // console.log(beer.dataValues.rating!=='0')
       userRatedBeers.push({
         id: beer.dataValues.beerId,
-        abv: 4.2,
-        geo: 1,
-        type: 7,
+        abv: beer.dataValues.beer.dataValues.abv,
+        geo: beer.dataValues.beer.dataValues.geoId,
+        type: beer.dataValues.beer.dataValues.typeId,
         score: parseInt(beer.dataValues.rating)
       })
     })
-    //let ratedBeersFiltered = await ratedBeers.filter(beer => {beer.dataValues.rating === '0'})
-    //console.log(ratedBeersFiltered)
-    res.json(userRatedBeers)
-    // const tensor = await Tensor()
-    // res.json(tensor)
+    const tensor = await Tensor(userRatedBeers)
+    res.json(tensor)
   } catch (err) {
     next(err)
   }
 })
-
-// // Get Single Beer
-// router.get('/:id', async (req, res, next) => {
-//   try {
-//     const beer = await Beer.findByPk(req.params.id)
-//     res.json(beer)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// // Get All Beers That A User Has Ranked
-// router.get('/ranked', async (req, res, next) => {
-//   try {
-//     const beer = await Beer.getRanked(req.body.ids)
-//     res.json(beer)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
