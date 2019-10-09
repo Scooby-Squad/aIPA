950594dee215e8b3865b220386fb7c3ddc5a30f3
Dominic Sciara <dominic.sciara@gmail.com>
2019-10-09 11:16:59 -0400

    resolving merge conflict

--- rn-client/components/QuestionsComponent.js
+++ rn-client/components/QuestionsComponent.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Modal } from 'react-native';
-
-// import {withApollo} from 'react-apollo'
-// import gql from 'graphql-tag'
 import RatingsList from './RatingsList';
 import SingleQuestion from './SingleQuestion';
 import RatingInput from './RatingInput';
@@ -10,50 +7,30 @@ import RatingInput from './RatingInput';
 const tempData = {
   questions: [
     {
+      id: 1,
       question: 'Beer 1'
     },
     {
-      question: 'Beer 2'
+      id: 2,
+      question: 'The 2nd Beer'
     }
   ]
 };
 
 const QuestionsComponent = props => {
-  // const query = gql`
-  //   query {
-  //     questions {
-  //       id
-  //       question
-  //       answer
-  //       category {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `
   const [quizData, setQuizData] = useState([]);
   const [currIdx, setCurrIdx] = useState(0);
-  const [enteredRating, setEnteredRating] = useState(0);
   const [isQuizFinished, setQuizFinished] = useState(false);
   const { returnHome } = props;
 
-  const ratingInputHandler = enteredText => {
-    setEnteredRating(enteredText);
-  };
-
-  const addRatingHandler = rating => {
+  const addRatingHandler = (rating = 0, skipped = false) => {
     if (rating.length === 0) return;
-    setEnteredRating(rating);
     const copyQuizData = quizData.map((question, index) => {
       if (index === currIdx) {
-        let skipped = false;
         return { ...question, rating, skipped };
-      } else {
-        return { ...question };
-      }
+      } else {return { ...question };}
     });
     setQuizData(copyQuizData);
-    setEnteredRating('');
     const nextIdx = currIdx + 1;
     if (nextIdx === quizData.length) {
       setQuizFinished(true);
@@ -67,17 +44,8 @@ const QuestionsComponent = props => {
     const fetchData = async () => {
       try {
         const data = tempData;
-        // const {data} = await props.client.query({
-        //   query,
-        //   fetchPolicy: 'no-cache'
-        // })
         if (data && data.questions) {
           setQuizData(data.questions);
-          // data.questions.forEach(question => {
-          //   console.log('question', question.question)
-          //   console.log('answer', question.answer)
-          // })
-          // console.log('questions', data.questions)
         }
       } catch (error) {
         console.error('error: ', error);
@@ -99,8 +67,6 @@ const QuestionsComponent = props => {
             </Text>
             <SingleQuestion quizData={quizData} currIdx={currIdx} />
             <RatingInput
-              ratingInputHandler={ratingInputHandler}
-              enteredRating={enteredRating}
               addRatingHandler={addRatingHandler}
               onCancel={props.onCancel}
             />
