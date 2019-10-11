f2cdf24f3fac9b95fcfafb2b0d7ed64ddcf14865
Dominic Sciara <dominic.sciara@gmail.com>
2019-10-11 14:17:52 -0400

    how about them styles

--- rn-client/components/AllBeerList/style-sheet.js
+++ rn-client/components/AllBeerList/style-sheet.js
@@ -1,27 +1,33 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center'
+    alignItems: "center",
+    marginHorizontal: 25
   },
   flatview: {
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    flexDirection: "row",
     paddingTop: 30,
-    borderRadius: 2
+    borderRadius: 2,
+    marginHorizontal: 25
   },
   rating: {
-    alignSelf: 'center',
-    flexDirection: 'column'
+    alignSelf: "center",
+    flexDirection: "column"
   },
   brewer: {
     opacity: 0.5
   },
   button: {
-    color: '#1a8cff'
+    color: "#1a8cff",
+    alignSelf: "center"
+  },
+  header: {
+    marginHorizontal: 45
   }
 });
 export const space =
-  '                                                                       ';
+  "                                                                       ";
 export default styles;
