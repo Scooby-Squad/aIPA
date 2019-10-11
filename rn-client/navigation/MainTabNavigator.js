3e9e526218fd814af00df48eeec44ad000463ace
Dominic Sciara <dominic.sciara@gmail.com>
2019-10-11 12:42:27 -0400

    fixed nav

--- rn-client/navigation/MainTabNavigator.js
+++ rn-client/navigation/MainTabNavigator.js
@@ -69,7 +69,8 @@ LinksStack.path = '';
 
 const SettingsStack = createStackNavigator(
   {
-    Settings: SettingsScreen
+    Settings: SettingsScreen,
+    SingleBeer: SingleBeerScreen
   },
   config
 );
